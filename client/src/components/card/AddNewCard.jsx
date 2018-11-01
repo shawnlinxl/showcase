ae7e9586d6658b564a550d1004436c0410aff70b
Shawn Lin <xiaoliang.lin@columbia.edu>
2018-10-31 23:32:58 -0400

    Dont need Icon import

--- client/src/components/card/AddNewCard.jsx
+++ client/src/components/card/AddNewCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Image, Placeholder, Icon } from 'semantic-ui-react';
+import { Card, Image, Placeholder } from 'semantic-ui-react';
 
 import AddNew from "../../assets/images/addnew.png"
 
